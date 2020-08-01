493805d4d60683792ec69f2c341369a6a3a5bca3
longyongtu <longyongtu13@qq.com>
2020-08-01 16:09:35 +0100

    1

---
+++ data_struct/queue/queue.h
@@ -0,0 +1,14 @@
+/********************************************************************************
+ *      Copyright:  (C) 2020 longyongtu<longyongtu13@qq.com>
+ *                  All rights reserved.
+ *
+ *       Filename:  queue.h
+ *    Description:  This head file 
+ *
+ *        Version:  1.0.0(01/08/20)
+ *         Author:  longyongtu <longyongtu13@qq.com>
+ *      ChangeLog:  1, Release initial version on "01/08/20 16:08:51"
+ *                 
+ ********************************************************************************/
+
+
