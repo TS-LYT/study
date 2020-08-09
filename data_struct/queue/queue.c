ca39251b3b9d953ccdb5816b04b4be25160e53fa
longyongtu <longyongtu13@qq.com>
2020-08-09 16:14:11 +0100

    1

--- data_struct/queue/queue.c
+++ data_struct/queue/queue.c
@@ -12,3 +12,76 @@
  ********************************************************************************/
 
 #include "queue.h"
+
+
+
+que_head_tail_t *que_init()
+{
+    que_head_tail_t *tmp=(que_head_tail_t *)malloc(sizeof(que_head_tail_t));
+    tmp->front=tmp->rear=NULL;
+    return tmp;
+}
+
+int que_empty(que_head_tail_t *tmp)
+{
+    return tmp->front=NULL;
+}
+
+int que_insert(que_head_tail_t *tmp, que_data data)
+{
+    if(tmp == NULL) 
+    {
+        printf("invaild argument\n");   
+        return -1;
+    }
+    
+    que_node_t *tmp1=(que_node_t *)malloc(sizeof(que_node_t));
+
+    tmp1->next=NULL;
+    tmp1->data=data;
+
+    if(tmp->rear == NULL)
+    {
+        tmp->rear =tmp1;
+        tmp->front =tmp1;
+    }
+    else
+    {
+        tmp->rear->next=tmp1;
+        tmp->rear=tmp1;
+    }
+    return -1;
+}
+
+int que_delete(que_head_tail_t *tmp)
+{
+    que_node_t      *tmp1=tmp->front;
+    if(tmp->front == NULL || tmp->rear == NULL) 
+    {
+        printf("queue don't have data\n");   
+        return -1
+    }
+    if(tmp->rear == trmp->front)
+    {
+        free(tmp->front);
+        tmp->rear == NULL;
+        tmp->front == NULL;
+    }
+    else
+    {
+        tmp->fornt=tmp->front->next;
+        free(tmp1);
+    }
+    return 0;
+}
+
+
+#if 1
+int main(int argc, char *argv[])
+{
+    
+    return 0;
+}
+
+
+#endif
--- data_struct/queue/queue.h
+++ data_struct/queue/queue.h
@@ -14,10 +14,65 @@
 #ifndef         _QUEUE_H
 #define         _QUEUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct que_data
+{
+    int         fd;
+}que_data_t;
+
+typedef struct que_node
+{
+    struct que_node     *next;
+    que_data            data;
+}que_node_t;
+
+typedef struct que_head_tail
+{
+    que_node_t          *front;
+    que_node_t          *rear;
+}que_head_tail_t
+
+/*! \brief creat head and tail point of queque
+ *
+ *  Detailed description of the function
+ 
+ * \return head and tail point struct
+ */
+que_head_tail_t *que_init();
 
 
+/*! \brief determine whether the queque is empty
+ *
+ *  Detailed description of the function
+ *
+ * \param  head and tail of queue
+ * \return empty is 1, not empty is nagetive
+ */
+int que_empty(que_head_tail_t *tmp);
 
 
+/*! \brief insert data into front of queue
+ *
+ *  Detailed description of the function
+ *
+ * \param head and tail of queue
+ * \param data you want to insert to queue
+ * \return 0 in case of successful, nagetive in case of erro
+ */
+int que_insert(que_head_tail_t *tmp, que_data_t *data);
+
+
+/*! \brief delete data int rear of queue
+ *
+ *  Detailed description of the function
+ *
+ * \param  head and tail of queue
+ * \return 0 in case of successful, nagetive in case of erro
+ */
+int que_delete(que_head_tail_t *tmp);
+       
 
 
 
